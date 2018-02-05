93f79c986c8b659336d32cc4b30affa9bab8883c
stathry <863245775@qq.com>
2018-02-06 00:32:07 +0800

    bootx

---
+++ src/main/java/org/stathry/bootx/BootxApplication.java
@@ -0,0 +1,12 @@
+package org.stathry.bootx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootxApplication.class, args);
+	}
+}
